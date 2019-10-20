bb054e7939e5e6830ae70a149bfc3880cbb07c07
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-10-20 09:00:06 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+/* eslint-disable react/jsx-indent-props */
+/* eslint-disable react/jsx-indent */
+import React, { Component } from 'react';
 import './App.css';
 
+const defaultTextColor = '#fff';
+const defaultStyle = { color: defaultTextColor };
+
+class Aggregate extends Component {
+	render() {
+		return (
+			<div
+				style={{ width: '40%', display: 'inline-block' }}
+				className="aggregate"
+			>
+				<h2 style={{ ...defaultStyle }}>Number text</h2>
+			</div>
+		);
+	}
+}
+
+class Filter extends Component {
+	render() {
+		return (
+			<div style={{ ...defaultStyle }}>
+				<img src="" alt="" />
+				<input type="text" />
+			</div>
+		);
+	}
+}
+
+class Playlist extends Component {
+	render() {
+		return (
+			<div style={{ ...defaultStyle, width: '25%' }}>
+				<img src="" alt="" />
+				<h3>Playlist Name</h3>
+				<ul>
+					<li>
+						Song 1
+					</li>
+					<li>
+						Song 2
+					</li>
+					<li>
+						Song 3
+					</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Title</h1>
+			<Aggregate />
+			<Aggregate />
+			<Filter />
+			<Playlist />
+			<Playlist />
+		</div>
+	);
 }
 
 export default App;
